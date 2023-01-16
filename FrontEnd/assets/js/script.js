671e56e6657346d165474f80f281cfa3d65dc50d
CharlesABJ <biadjeu.charles@orange.fr>
2023-01-16 23:46:47 +0100

    Javascript : boucle créée avec fetch pour faire apparaitre le titre des éléments Html CSS : création brouillon page login

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,4 +1,12 @@
 fetch("http://localhost:5678/api/works")
-.then(response => response.json)
-.then(data => console.log(data, 'test'))
-.catch(error => console.log(error, "Error"));
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].title);
+
+            // add more code here
+        }
+    })
+    .catch(error => console.log(error, "Error"));
+
+
