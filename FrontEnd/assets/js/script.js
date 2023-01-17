7e8f515adcc0f294c9c71a193d26f652aab1c780
CharlesABJ <biadjeu.charles@orange.fr>
2023-01-17 21:26:54 +0100

    JS : intégration des images et des titres des travaux de l'api en HTML

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -2,11 +2,21 @@ fetch("http://localhost:5678/api/works")
     .then(response => response.json())
     .then(data => {
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i].title);
+            let galleryGrid = document.querySelector(".galleryGrid");
+            let figure = document.createElement("figure");
+            let img = document.createElement("img");
+            let figcaption = document.createElement("figcaption")
+
+            galleryGrid.append(figure);
+            figure.append(img, figcaption);
+            
+            img.setAttribute("src", data[i].imageUrl)
+            img.setAttribute("alt", data[i].title)
+            img.setAttribute("crossorigin", "anonymous")
+            figcaption.innerHTML = data[i].title;
 
             // add more code here
         }
     })
-    .catch(error => console.log(error, "Error"));
-
+    .catch(error => console.log(error, "Error mon ami"));
 
