689751b9200deeb8d82f3a40da133d717fef4bae
CharlesABJ <biadjeu.charles@orange.fr>
2023-01-18 19:47:26 +0100

    HTML : - Ajout d'une classe filterElement pour les filtres - La classe active est devenue un ID
    CSS: Ajout de l'ID inactive pour le JS
    
    JS : Ajout et configuration des evenements des filtres sur chaque clique. Tout semble fonctionnel, attente de confirmation avant de poursuivre.

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,5 +1,13 @@
 // Récupération des données de l'API
 
+
+let all = document.querySelector(".all")
+let objects = document.querySelector(".objects")
+let appartments = document.querySelector(".appartments")
+let restaurants = document.querySelector(".restaurants")
+let filterElement = document.querySelectorAll(".filterElement")
+
+
 fetch("http://localhost:5678/api/works")
     .then(response => response.json())
     .then(data => {
@@ -12,37 +20,59 @@ fetch("http://localhost:5678/api/works")
             galleryGrid.append(figure);
             figure.append(img, figcaption);
 
+            figure.setAttribute("data-category-id", data[i].category.id)
             img.setAttribute("src", data[i].imageUrl)
             img.setAttribute("alt", data[i].title)
             img.setAttribute("crossorigin", "anonymous")
             figcaption.innerHTML = data[i].title;
 
+            all.addEventListener("click", () => {
+                filterElement.forEach( a=> {
+                    a.setAttribute("id", "inactive");
+                });
+                all.setAttribute("id", "active")
+                figure.style.display = "block"
+            })
+
+            objects.addEventListener("click", () => {
+                filterElement.forEach( a=> {
+                    a.setAttribute("id", "inactive");
+                });
+                objects.setAttribute("id", "active")
+                figure.style.display = "block";
+                if (figure.getAttribute("data-category-id") !== "1") {
+                    figure.style.display = "none";
+                }
+
+            })
+
+            appartments.addEventListener("click", () => {
+                filterElement.forEach( a=> {
+                    a.setAttribute("id", "inactive");
+                });
+                appartments.setAttribute("id", "active");
+                figure.style.display = "block";
+                if (figure.getAttribute("data-category-id") !== "2") {
+                    figure.style.display = "none";
+
+                }
+            })
+
+            restaurants.addEventListener("click", () => {
+                filterElement.forEach( a=> {
+                    a.setAttribute("id", "inactive");
+                });
+                restaurants.setAttribute("id", "active")
+                figure.style.display = "block";
+                if (figure.getAttribute("data-category-id") !== "3") {
+                    figure.style.display = "none";
+                }
+            })
             // add more code here
         }
     })
     .catch(error => console.log(error, "Error mon ami"));
 
 
-// Mise en place des filtres
-
-let all = document.querySelector(".all")
-let objects = document.querySelector(".objects")
-let appartments = document.querySelector(".appartments")
-let restaurants = document.querySelector(".restaurants")
-
-all.addEventListener("click", () => {
-
-})
 
 
-objects.addEventListener("click", () => {
-
-})
-
-appartments.addEventListener("click", () => {
-
-})
-
-restaurants.addEventListener("click", () => {
-
-})
