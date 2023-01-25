cccc558784c4cfcbdbddeb29886ad80da6959274
CharlesABJ <biadjeu.charles@orange.fr>
2023-01-25 20:18:40 +0100

    Javascript - LOGIN : Simplification du code, ajout variable eyes pour agir directement sur les boutons
    A faire remplacer les données textuels mail et mdp par celle de l'Api
    
    S'occuper de la partie javascript de la page principale, filtres + changement de couleur à finir demain

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,34 +1,36 @@
-// associer ce fichier à la page login.html
-// log("login-chargé")
-console.log("login-chargé");
 let form = document.querySelector("form");
-let email = document.getElementById("mail");
-let password = document.getElementById("password");
+let emailInput = document.getElementById("mail");
+let passwordInput = document.getElementById("password");
 let eyeClosed = document.querySelector(".eye-closed");
 let eyeOpen = document.querySelector(".eye-open");
-let eyes = document.querySelectorAll("i");
-// Détecter quand le formulaire est soumis/submit
-form.addEventListener("submit", () => {
-  if (email.value === "sophie.bluel@test.tld" && password.value === "S0phie") {
+let eyes = document.querySelectorAll(".eyes");
+
+let errorConnect = document.querySelector(".errorConnect");
+
+form.addEventListener("submit", (e) => {
+  if (
+    emailInput.value === "sophie.bluel@test.tld" &&
+    passwordInput.value === "S0phie"
+  ) {
     console.log("Tout est bon");
+    errorConnect.classList.add("hidden");
   } else {
-    console.log("Mot de passe ou identifiant incorrect");
+    e.preventDefault()
+    errorConnect.classList.remove("hidden");
+    errorConnect.innerHTML = "Mot de passe ou identifiant incorrect";
+    passwordInput.style.outlineColor = "red";
+    emailInput.style.outlineColor = "red";
   }
 });
 
-// à ce moment là récuperer les valeur des input dans 2 console.log
-
-// Modification
-
-eyeClosed.addEventListener("click", () => {
-  eyeClosed.classList.toggle("hidden");
-  eyeOpen.classList.toggle("hidden");
-  password.type = "text";
+eyes.forEach((eye) => {
+  eye.addEventListener("click", () => {
+    eyeClosed.classList.toggle("hidden");
+    eyeOpen.classList.toggle("hidden");
+    if (password.type === "password") {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
+  });
 });
-
-eyeOpen.addEventListener("click", () => {
-  eyeClosed.classList.toggle("hidden");
-  eyeOpen.classList.toggle("hidden");
-  password.type = "password";
-});
-// Chercher un moyen de raccourcir ce code
