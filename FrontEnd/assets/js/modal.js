fa036d3a8876fc4eb679e8f307287f78f5ea4d22
CharlesABJ <biadjeu.charles@orange.fr>
2023-02-02 15:40:15 +0100

    Lorsqu'on se deconnecte le token disparait et la page index revient à son état d'origine

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -3,19 +3,18 @@ let hiddenElements = document.querySelectorAll(".hidden")
 let login = document.querySelector(".login")
 let logout = document.querySelector(".logout")
 if (userToken) {
-    console.log("yesss");
     for( let element of hiddenElements){
         element.classList.remove("hidden")
     }
     login.style.display="none";
-} else{
-    console.log("nooo");
 }
 
 logout.addEventListener("click", function(){
     logout.style.display="none";
     login.style.display="block";
+    sessionStorage.removeItem("token")
     for( let element of hiddenElements){
         element.classList.add("hidden")
     }
+    location.href= "index.html"
 })
