bb81b476acf23a317e107566befbdd6396d8141f
CharlesABJ <biadjeu.charles@orange.fr>
2023-02-02 12:43:55 +0100

    vulgarisation de deconnection en appuyant sur logout

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,6 +1,7 @@
 let userToken = sessionStorage.getItem("token"); 
 let hiddenElements = document.querySelectorAll(".hidden")
 let login = document.querySelector(".login")
+let logout = document.querySelector(".logout")
 if (userToken) {
     console.log("yesss");
     for( let element of hiddenElements){
@@ -10,3 +11,11 @@ if (userToken) {
 } else{
     console.log("nooo");
 }
+
+logout.addEventListener("click", function(){
+    logout.style.display="none";
+    login.style.display="block";
+    for( let element of hiddenElements){
+        element.classList.add("hidden")
+    }
+})
