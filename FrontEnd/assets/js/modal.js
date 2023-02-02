f0f4b6c64a990f72bd45001ac8d4a7cb8bd829a7
CharlesABJ <biadjeu.charles@orange.fr>
2023-02-02 12:27:45 +0100

    ajout sessionStorage dans modal.js, lorsque le token est présent le mode edition apparaît

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,3 +1,12 @@
-// if (sessionStorage.getItem("token", data.token)) {
-//     console.log("yesss");
-// }
+let userToken = sessionStorage.getItem("token"); 
+let hiddenElements = document.querySelectorAll(".hidden")
+let login = document.querySelector(".login")
+if (userToken) {
+    console.log("yesss");
+    for( let element of hiddenElements){
+        element.classList.remove("hidden")
+    }
+    login.style.display="none";
+} else{
+    console.log("nooo");
+}
