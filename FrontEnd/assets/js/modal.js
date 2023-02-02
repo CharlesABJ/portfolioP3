b99560c4e162cb57109a4f985611895cbe7341eb
CharlesABJ <biadjeu.charles@orange.fr>
2023-02-02 16:01:15 +0100

    ajout des variables pour l'activation des modales et pour la confirmation des changements

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,7 +1,13 @@
 let userToken = sessionStorage.getItem("token"); 
-let hiddenElements = document.querySelectorAll(".hidden")
 let login = document.querySelector(".login")
 let logout = document.querySelector(".logout")
+let hiddenElements = document.querySelectorAll(".hidden")
+let editPortrait = document.querySelector(".edit-portrait")
+let editPresentation = document.querySelector(".edit-presentation")
+let editWorks = document.querySelector(".edit-works")
+let publishChanges = document.querySelector(".edition-mode button")
+
+
 if (userToken) {
     for( let element of hiddenElements){
         element.classList.remove("hidden")
@@ -17,4 +23,20 @@ logout.addEventListener("click", function(){
         element.classList.add("hidden")
     }
     location.href= "index.html"
+})
+
+editPortrait.addEventListener("click", function(){
+    console.log("portrait");
+})
+
+editPresentation.addEventListener("click", function(){
+    console.log("presentation");
+})
+
+editWorks.addEventListener("click", function(){
+    console.log("works");
+})
+
+publishChanges.addEventListener("click", function(){
+    console.log("youhouu");
 })
