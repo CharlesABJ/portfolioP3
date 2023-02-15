8aa66145fc52faad85b41dce3e48e2ae3488021f
CharlesABJ <biadjeu.charles@orange.fr>
2023-02-15 18:09:27 +0100

    fichier hamburger-menu-responsive.js nettoyé

--- FrontEnd/assets/js/hamburger-menu-responsive.js
+++ FrontEnd/assets/js/hamburger-menu-responsive.js
@@ -5,13 +5,13 @@ let h1Responsive = document.querySelector(".h1-responsive");
 let bubbleNav = document.querySelector(".bubble-nav");
 let navElements = document.querySelectorAll("header nav ul li a");
 
+//=======================================================================
+
+
 // Menu de navigation responsive
-
-function toggleNav() {
+bubbleNav.addEventListener("click", function () {
   header.classList.toggle("background-responsive");
-}
-
-bubbleNav.addEventListener("click", toggleNav);
+});
 
 for (let element of navElements) {
   element.addEventListener("click", function () {
